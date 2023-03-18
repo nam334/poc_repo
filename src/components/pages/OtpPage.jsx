dbf37e4970b8fee7369f508bd0b7a8db63f0eb0a
nam334 <namratadas334@gmail.com>
2023-03-18 21:37:05 +0530

    added loaders and success page

--- src/components/pages/OtpPage.jsx
+++ src/components/pages/OtpPage.jsx
@@ -2,10 +2,10 @@ import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { validateOtp } from '../../loginSlice'
 import { useNavigate } from "react-router-dom";
-
+import {MdOutlineMobileFriendly} from 'react-icons/md'
 const OtpPage = () => {
   const [timer, setTimer] = useState(10)
-  const [reset, setReset] = useState(false)
+  const [loader, setLoader] = useState(false)
   const [firstNum, setfirstNum] = useState('')
   const [secondNum, setsecondNum] = useState('')
   const [thirdNum, setthirdNum] = useState('')
@@ -23,12 +23,7 @@ const OtpPage = () => {
      setsubmit(true)
    
     }
-    // if(firstNum ==='' && secondNum ==='' && thirdNum ==='' && fourthNum ==='' ){
-    //   setReset(false)
-    // }
-    
-    //console.log(firstNum)
-  },[firstNum, secondNum, thirdNum, fourthNum, dispatch,timer, border, reset])
+  },[firstNum, secondNum, thirdNum, fourthNum, dispatch,timer, border])
 
   
   
@@ -67,21 +62,29 @@ const OtpPage = () => {
 
   
   const resendHandler = () => {
-    setfirstNum('')
-    setsecondNum('')
-    setthirdNum('') 
-    setfourthNum('')
-   setTimer(10)
-   setborder(false)
+    setLoader(true)
+    setTimeout(()=>{
+      setLoader(false)
+      setfirstNum('')
+      setsecondNum('')
+      setthirdNum('') 
+      setfourthNum('')
+     setTimer(10)
+     setborder(false)
+    },1000)
+  
  
   }
   return (
     <>
-    <div className='flex justify-center bg-slate-100 items-center h-screen'>
+    
+    <div className='flex justify-center bg-pink-50 items-center h-screen'>
     <div className="w-full max-w-xs ">
-  <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-    <div className="mb-4">
-      <label className="block text-blue-900 text-sm  font-bold mb-2" htmlFor="username">
+    
+  <form className="bg-white shadow-lg rounded px-8 py-8 mb-4 flex flex-col items-start">
+  <MdOutlineMobileFriendly size={40} className="text-blue-900" />
+    <div className="">
+      <label className="block text-blue-900 text-md font-semibold my-4" htmlFor="username">
         Verify with OTP
       </label>
       
@@ -89,33 +92,35 @@ const OtpPage = () => {
     <div className="mb-3">
      
       <input type="text" className = {` focus:shadow-outline rounded-sm border-2 
-      text-gray-500 leading-tight focus:outline-none focus:shadow-outline w-5  p-1  mx-2    
+      text-gray-500 leading-tight focus:outline-none focus:shadow-outline w-5  p-1 mr-2       
       ${result ? `border-slate-500`  : ( border ? `border-red-500`: `border-slate-500`)  }`} 
        value={firstNum}
        onChange={(e)=> setfirstNum(e.target.value)}/>
        <input type="text" className = {` focus:shadow-outline rounded-sm border-2 
-      text-gray-500 leading-tight focus:outline-none focus:shadow-outline w-5  p-1  mx-2    
+      text-gray-500 leading-tight focus:outline-none focus:shadow-outline w-5  p-1 mr-2       
       ${result ? `border-slate-500`  : ( border ? `border-red-500`: `border-slate-500`)  }`}  value={secondNum}
        onChange={(e)=> setsecondNum(e.target.value)}/>
        <input type="text" className = {` focus:shadow-outline rounded-sm border-2 
-      text-gray-500 leading-tight focus:outline-none focus:shadow-outline w-5  p-1  mx-2    
+      text-gray-500 leading-tight focus:outline-none focus:shadow-outline w-5  p-1 mr-2       
       ${result ? `border-slate-500`  : ( border ? `border-red-500`: `border-slate-500`)  }`}  value={thirdNum}
        onChange={(e)=> setthirdNum(e.target.value)}/>
      <input type="text"className = {` focus:shadow-outline rounded-sm border-2 
-      text-gray-500 leading-tight focus:outline-none focus:shadow-outline w-5  p-1  mx-2    
+      text-gray-500 leading-tight focus:outline-none focus:shadow-outline w-5  p-1 mr-2       
       ${result ? `border-slate-500`  : ( border ? `border-red-500`: `border-slate-500`)  }`}  value={fourthNum}
        onChange={(e)=> setfourthNum(e.target.value)}/>
     </div>
-    <div>
+    <div className='flex gap-6'>
       <p className='font-semibold text-sm text-slate-400'>
-        {/* {submit ? (reset ? <span className='text-slate-600 cursor-pointer' onClick={resendHandler}>Resend OTP</span> :  
-        <span className='text-slate-400 pointer-events-none'>Resend OTP in {timer} secs</span>): 
-        <span className='text-slate-400 pointer-events-none'>Resend OTP in {timer} secs</span> } */}
-        {/* { reset ? <span className='text-slate-600 cursor-pointer' onClick={resendHandler}>Resend OTP</span> :  
-       submit &&  <span className='text-slate-400 pointer-events-none'>Resend OTP in {timer} secs</span>} */}
-       {timer > 0 ? <span className='text-slate-400 pointer-events-none'>Resend OTP in {timer} secs</span> : 
-        <span className='text-slate-600 cursor-pointer' onClick={resendHandler}>Resend OTP</span> }
+       {timer > 0 ? <span className='text-pink-400 pointer-events-none'>Resend OTP in {timer} secs</span> : 
+        <span className='text-pink-600 font-bold cursor-pointer' onClick={resendHandler}>Resend OTP</span> }
+        
       </p>
+     
+      {loader && <svg aria-hidden="true" class="inline w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-pink-400" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+    </svg>}
+   
     </div>
    
   </form>
--- src/components/pages/SuccessPage.jsx
+++ src/components/pages/SuccessPage.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import {BsEmojiSmile} from "react-icons/bs"
 const SuccessPage = () => {
   return (
-    <div>successPage</div>
+    <div className='bg-pink-50 h-screen flex justify-center items-center gap-3'>
+      <h3 className='text-blue-900 text-3xl'>Logged in successfully..!! </h3><BsEmojiSmile size={40} className='text-pink-600'/>
+    </div>
   )
 }
 
